0a7e4da095ee2a157f59737b39d67de6200021b3
Ankush-Kumar-Cloud <ankushkumary77@gmail.com>
2026-05-07 03:49:30 +0530

    setup backend

---
+++ server.js
@@ -0,0 +1,48 @@
+import express from "express";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// import { scrapeNews } from "./src/utils/scraper.js";
+// import connectDB from "./src/config/db.js";
+
+// import scrapeRoutes from "./src/routers/srcapeRoute.js";
+// import storyRoutes from "./src/routers/storyRouter.js";
+// import authRoutes from "./src/routers/authRoutes.js";
+
+
+
+
+// dotenv.config();
+
+const app = express();
+
+// middleware
+// app.use(cors());
+app.use(express.json());
+
+// DB
+// connectDB();
+
+
+// routes (we’ll add later)
+app.get("/", (req, res) => {
+  res.send("API running...");
+});
+
+
+// app.use("/api", scrapeRoutes);
+
+// app.use("/api", storyRoutes);
+
+// app.use("/api", authRoutes)
+
+
+
+
+const PORT = process.env.PORT || 5000;
+
+
+
+app.listen(PORT, async () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+  // await scrapeNews(); // auto run
+});
