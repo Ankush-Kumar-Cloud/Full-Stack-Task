cf56f89a44089fad35809f05de9a8d9411108518
Ankush-Kumar-Cloud <ankushkumary77@gmail.com>
2026-05-07 14:04:44 +0530

    add story APIs

---
+++ src/controllers/storyController.js
@@ -0,0 +1,51 @@
+import Story from "../models/storyModel.js"
+import User from "../models/userModel.js"
+
+export const getStories = async (req, res) => {
+  try {
+    const stories = await Story.find().sort({ points: -1 });
+    res.json(stories);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching stories" });
+  }
+};
+
+export const getStoryById = async (req, res) => {
+  try {
+    const story = await Story.findById(req.params.id);
+    res.json(story);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching story" });
+  }
+};
+
+
+
+
+
+//Bookmark
+
+export const toggleBookmark = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+
+    const storyId = req.params.id;
+
+    const isBookmarked = user.bookmarks.includes(storyId);
+
+    if (isBookmarked) {
+      user.bookmarks = user.bookmarks.filter(
+        (id) => id.toString() !== storyId
+      );
+    } else {
+      user.bookmarks.push(storyId);
+    }
+
+    await user.save();
+
+    res.json({ bookmarks: user.bookmarks });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Bookmark error" });
+  }
+};
---
+++ src/routers/storyRouter.js
@@ -0,0 +1,16 @@
+import { getStories, getStoryById, toggleBookmark } from "../controllers/storyController.js";
+import express from "express"
+import { protect } from "../middleware/authMiddleware.js";
+
+const storyRoutes = express.Router();
+
+storyRoutes.get("/stories", getStories);
+storyRoutes.get("/stories/:id", getStoryById);
+
+
+
+
+
+storyRoutes.post("/stories/:id/bookmark", protect, toggleBookmark);
+
+export default storyRoutes;
